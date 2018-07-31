36e1295bc25ea5c20cec7a2f575d7622fded71fb
tqcenglish <tqcenglish@gmail.com>
2018-07-31 22:36:40 +0800

    update config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 
-fs_conf_path = 'C:/Program Files (x86)/FreeSWITCH/conf'#'/usr/local/freeswitch/conf'
+fs_conf_path = '/usr/local/Cellar/freeswitch/1.6.20/etc/freeswitch'#'/usr/local/freeswitch/conf'
 
 odbc_credentials = "freepybx:freepybx:secretpass1"
 
@@ -9,7 +9,7 @@ odbc_credentials = "freepybx:freepybx:secretpass1"
 event_socket_conf = {
                      'host': '127.0.0.1',
                      'port': 8021,
-                     'pwd': '520999'
+                     'pwd': 'ClueCon'
                      }
 
 fs_db_path = 'C:/Program Files (x86)/FreeSWITCH/db'
